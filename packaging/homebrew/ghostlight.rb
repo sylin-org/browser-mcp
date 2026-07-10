7d666b3c92c660874e1c0d8fc36056a5c7566ba8
Leo Botinelly <leonardo.botinelly@gmail.com>
2026-07-10 08:33:20 -0400

    chore(release): fill v0.5.4 package-manager checksums

--- packaging/homebrew/ghostlight.rb
+++ packaging/homebrew/ghostlight.rb
@@ -10,16 +10,16 @@ class Ghostlight < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sylin-org/ghostlight/releases/download/v#{version}/ghostlight-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "TODO-SHA256-FROM-RELEASE-ASSET"
+      sha256 "58875dadac6ba25c483a921915cc10e6f4747607813129361b98754143e19343"
     else
       url "https://github.com/sylin-org/ghostlight/releases/download/v#{version}/ghostlight-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "TODO-SHA256-FROM-RELEASE-ASSET"
+      sha256 "40aaf27dd91727f3ea4095b88ae2284b31be8915ca9dcc8e15f8c2f888103b79"
     end
   end
 
   on_linux do
     url "https://github.com/sylin-org/ghostlight/releases/download/v#{version}/ghostlight-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "TODO-SHA256-FROM-RELEASE-ASSET"
+    sha256 "df9b5023bf8bd0bfebd360f0e62d8d8d67e4848b9f7e3e3dbc3def661fd83678"
   end
 
   def install
